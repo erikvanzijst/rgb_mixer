403008d8f4234195fbb23765fe973ba2fa924fdb
Erik van Zijst <erik.van.zijst@gmail.com>
2021-04-08 21:41:39 -0700

    Make PWM duty cycle width a proper parameter

--- src/pwm.v
+++ src/pwm.v
@@ -1,15 +1,16 @@
 `default_nettype none
 `timescale 1ns/1ns
 
-module pwm (
-    input wire clk,
-    input wire reset,
-    output wire out,
-    input wire [7:0] level
+module pwm
+    #(parameter WIDTH = 8)
+    (
+        input wire clk,
+        input wire reset,
+        output wire out,
+        input wire [7:0] level
     );
 
     parameter INVERT = 0;
-    parameter WIDTH = 8;
 
     reg [WIDTH-1:0] counter;
     wire duty = counter < level;
